99c42f42be3d72b84b14341e214279c1be0acb68
Lazar Miseljic <lazar@spinview.io>
2022-04-18 22:29:30 +0200

    square analytics in a round hole...

---
+++ components/analytics/Matomo.js
@@ -0,0 +1,27 @@
+import Script from 'next/script'
+
+import siteMetadata from '@/data/siteMetadata'
+
+const MatomoScript = () => {
+  return (
+    <>
+      <Script strategy="lazyOnload" id="plausible-script">
+        {`
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//stats.fuzzylogic.ltd/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+        `}
+      </Script>
+    </>
+  )
+}
+
+export default MatomoScript
