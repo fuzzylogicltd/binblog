0ddf50de36bc9f970c4d7fb323136e0aa87a5641
Lazar Miseljic <lazar@spinview.io>
2022-04-18 23:03:13 +0200

    More analytics stuff

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -14,7 +14,7 @@ const Analytics = () => {
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
-      {isProduction && <Matomo />}
+      {<Matomo />}
     </>
   )
 }
